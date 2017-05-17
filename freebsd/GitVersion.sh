9bdfe11b27a79998b6e2c9bd76c02a102296e456
Ken Merry <ken@kdm.org>
2017-05-17 12:27:40 -0600

    Set GIT_VERSION to Unknown if we can't detect revision control.
    If git or perforce aren't available, just define the version as
    Unknown.

--- freebsd/GitVersion.sh
+++ freebsd/GitVersion.sh
@@ -10,4 +10,6 @@ p4 cstat ... > /dev/null 2>&1
 if [ $? -eq 0 ]; then
 	P4VER=`p4 cstat ... |grep change |awk '{print $NF}'`
 	echo "#define GIT_VERSION " \"$P4VER\"
+else
+	echo "#define GIT_VERSION " \"Unknown\"
 fi
