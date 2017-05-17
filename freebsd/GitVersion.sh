1edca5471f7d2603f2fbef96b3949ebd26359e47
Ken Merry <ken@kdm.org>
2017-05-17 12:26:18 -0600

    Add Perforce version detection.
    Make an attempt to determine the git version, and if we aren't
    using git, make an attempt to determine the current Perforce change
    of the source.

--- freebsd/GitVersion.sh
+++ freebsd/GitVersion.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+
 GITVER=`git describe --dirty`
-echo "#define GIT_VERSION " \"$GITVER\"
+if [ $? -eq 0 ]; then
+	echo "#define GIT_VERSION " \"$GITVER\"
+	exit 0
+fi
+
+p4 cstat ... > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	P4VER=`p4 cstat ... |grep change |awk '{print $NF}'`
+	echo "#define GIT_VERSION " \"$P4VER\"
+fi
